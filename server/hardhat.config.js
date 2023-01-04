3a653c0552dd2e472680d9120f2b5ba5eaaef888
Hunzalaqamar <hunzla.qamar@gmail.com>
2023-01-05 01:55:50 +0500

    A New Horizon

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -7,9 +7,9 @@ module.exports = {
 
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/VFzv69UEIT2Kp4-i7hcGfCZueMA4x26_",
+      url: "Your URL from Alchemy",
       accounts: [
-        "0723d714176d6ef5c87f29d0ea57135c7c757c912665f6ed6a9101cd73545d3b",
+        "Your metamask account private key",
       ],
     },
   },
